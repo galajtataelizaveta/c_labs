170df22062b52619daf32fe09a2ca9c848004e53
kiradeku <fatkhutdinov@edu.surgu.ru>
2022-10-19 14:06:20 +0500

    add lab1

---
+++ lab1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+     int a, b, x, c, k;
+     printf("Enter a -> ");
+     scanf("%d", &a);
+
+     printf("Enter b -> ");
+     scanf("%d", &b);
+
+     printf("Enter x -> ");
+     scanf("%d", &c);
+     if(k > c)
+         
+        if (a < b)
+            printf(c = a + x)
+        else:
+            printf(k = a - x)
+     printf (k)
+     else: 
+         printf (c)
+
+    return 0;
+}
